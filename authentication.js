f43ad648e0ab70ead50aacc34e78d7ec1a85e711
Shai Schechter <shai126@gmail.com>
2020-11-07 16:56:39 +0000

    Tidy

--- authentication.js
+++ authentication.js
@@ -1,16 +1,15 @@
-'use strict';
+// Befores
 
-// You want to make a request to an endpoint that is either specifically designed
-// to test auth, or one that every user will have access to. eg: `/me`.
-// By returning the entire request object, you have access to the request and
-// response data for testing purposes. Your connection label can access any data
-// from the returned response using the `json.` prefix. eg: `{{json.username}}`.
-const test = (z, bundle) =>
-  z.request({ url: 'https://api.github.com/user' });
+const includeApiKey = (request, z, bundle) => {
+  if (bundle.authData.accessToken) {
+    request.headers.Authorization = `Basic ${Buffer.from(`:${bundle.authData.accessToken}`).toString('base64')}`;
+  }
 
-// This function runs after every outbound request. You can use it to check for
-// errors or modify the response. You can have as many as you need. They'll need
-// to each be registered in your index.js file.
+  return request;
+};
+
+// Afters
+
 const handleBadResponses = (response, z, bundle) => {
   if (response.status === 401) {
     throw new z.errors.Error(
@@ -24,24 +23,12 @@ const handleBadResponses = (response, z, bundle) => {
   return response;
 };
 
-// This function runs before every outbound request. You can have as many as you
-// need. They'll need to each be registered in your index.js file.
-const includeApiKey = (request, z, bundle) => {
-  if (bundle.authData.accessToken) {
-    request.headers.Authorization = `Basic ${Buffer.from(`:${bundle.authData.accessToken}`).toString('base64')}`;
-  }
-
-  return request;
-};
+// Here we go
 
 module.exports = {
   config: {
-    // "custom" is the catch-all auth type. The user supplies some info and Zapier can
-    // make authenticated requests with it
     type: 'custom',
 
-    // Define any input app's auth requires here. The user will be prompted to enter
-    // this info when they connect their account.
     fields: [
       {
         key: 'accessToken',
@@ -50,17 +37,8 @@ module.exports = {
       },
     ],
 
-    // The test method allows Zapier to verify that the credentials a user provides
-    // are valid. We'll execute this method whenver a user connects their account for
-    // the first time.
-    test,
+    test: (z, bundle) => z.request({ url: 'https://api.github.com/user' }),
 
-    // This template string can access all the data returned from the auth test. If
-    // you return the test object, you'll access the returned data with a label like
-    // `{{json.X}}`. If you return `response.data` from your test, then your label can
-    // be `{{X}}`. This can also be a function that returns a label. That function has
-    // the standard args `(z, bundle)` and data returned from the test can be accessed
-    // in `bundle.inputData.X`.
     connectionLabel: '{{json.login}}',
   },
   befores: [includeApiKey],
--- index.js
+++ index.js
@@ -1,30 +1,20 @@
-const {
-  config: authentication,
-  befores = [],
-  afters = [],
-} = require('./authentication');
+const authentication = require('./authentication');
 
 const reverse = require('./creates/reverse');
 
 module.exports = {
-  // This is just shorthand to reference the installed dependencies you have.
-  // Zapier will need to know these before we can upload.
   version: require('./package.json').version,
   platformVersion: require('zapier-platform-core').version,
 
-  authentication,
+  authentication: authentication.config,
 
-  beforeRequest: [...befores],
+  beforeRequest: [...authentication.befores],
+  afterResponse: [...authentication.afters],
 
-  afterResponse: [...afters],
-
-  // If you want your trigger to show up, you better include it here!
   triggers: {},
 
-  // If you want your searches to show up, you better include it here!
   searches: {},
 
-  // If you want your creates to show up, you better include it here!
   creates: {
     [reverse.key]: reverse
   },
